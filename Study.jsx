874b79433a375c53a1eedd76c1b7abb174428688
Asmina <161222308+Asminamohamed@users.noreply.github.com>
2024-08-07 23:04:11 +0530

    Add files via upload

---
+++ Study.jsx
@@ -0,0 +1,20 @@
+export const Study = () => {
+    return(
+        <div className="Study">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Age</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>isMarried</th>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    )
+}
---
+++ UserCard.jsx
@@ -0,0 +1,33 @@
+
+
+
+function User() {
+    return<div className="Card-Container">
+    <span className="pro">ONLINE</span> 
+    <h3>Aszinyara</h3>
+    <p>Frond End Devolper</p>
+    <div>
+    <button>Message</button>
+    <button>Follow</button>
+    </div>
+    <div>
+        <h5>Skills</h5>   
+        <p>Engineering from oxford university</p> 
+        <p>UI/UX</p>
+        <p>Areospace</p>
+        <p>Content Creator</p>
+        </div>
+    </div>
+}
+export const UserCard =() => {
+    return (
+         
+            <User />
+           
+    )
+}
+
+
+
+
+
